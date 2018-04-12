6fc60ceff7584d6eacd5b8723f0a6f10364984e4
shoxruxX <shoxruxxudoynazarov9695@gmail.com>
2018-04-12 19:51:51 +0500

    Shoxrux931-17

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -1,8 +1,13 @@
 package controllers
 
+import java.util.Date
+
 import com.typesafe.scalalogging.LazyLogging
 import javax.inject._
+<<<<<<< Updated upstream
 
+=======
+>>>>>>> Stashed changes
 import play.api.libs.json.Json
 import play.api.mvc._
 import views.html._
@@ -19,9 +24,66 @@ class HomeController @Inject()(val controllerComponents: ControllerComponents,
   extends BaseController with LazyLogging {
 
   val LoginSessionKey = "login.key"
+<<<<<<< Updated upstream
 
   def index = Action { implicit request => {
     request.session.get(LoginSessionKey).map { session =>
+=======
+  case class Users(email: String, login: String, createdAt: Date)
+  var UsersList = List.empty[Users]
+  val user1 = Users(
+    email = "asdf@asd.dd",
+    login = "asdf",
+    createdAt = new Date
+  )
+  val user2 = Users(
+      email = "shoxrux0390.@mail.ru",
+      login = "shoxrux123",
+      createdAt = new Date
+  )
+  val user3 = Users(
+    email = "shoxruxxudoynazarov0390.@mail.ru",
+    login = "banana123",
+    createdAt = new Date
+  )
+  val user4 = Users(
+    email = "xudoynazarov0390.@mail.ru",
+    login = "banana0390",
+    createdAt = new Date
+  )
+  val user5 = Users(
+    email = "java0390.@gmail.com",
+    login = "scala123",
+    createdAt = new Date
+  )
+  val user6 = Users(
+    email = "scala.@gmail.com",
+    login = "scala321",
+    createdAt = new Date
+  )
+  val user7 = Users(
+    email = "javascala931.@mail.com",
+    login = "person931",
+    createdAt = new Date
+  )
+  val user8 = Users(
+    email = "banana0390.@bk.ru",
+    login = "apple123",
+    createdAt = new Date
+  )
+  val user9 = Users(
+    email = "apple931.@mail.ru",
+    login = "cherry",
+    createdAt = new Date
+  )
+  val user10 = Users(
+    email = "person.@gmail.com",
+    login = "user123",
+    createdAt = new Date
+  )
+  def index = Action { implicit  request => {
+    request.session.get(LoginSessionKey).map{ session =>
+>>>>>>> Stashed changes
       logger.info(s"session: $session")
       Ok(indexTemplate())
     }.getOrElse {
@@ -39,12 +101,21 @@ class HomeController @Inject()(val controllerComponents: ControllerComponents,
   }
   }
 
+
   def logout = Action { implicit request => {
     Redirect(routes.HomeController.index()).withSession(
       request.session - LoginSessionKey
     )
   }
   }
+
+//  def report = Action {
+//    val usersList = List(user1,user2,user3,user4,user5,user6,user7,user8,user9,user10)
+//    usersList.map{ users =>
+//      Ok(Json.toJson(Map("users" -> users)))
+//    }
+//  }
+
 
   def loginPost = Action { implicit request => {
     val formParam = request.body.asFormUrlEncoded

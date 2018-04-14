1044d5b4fa090f079dc9027de187c4a87e6e3c9a
shoxruxX <shoxruxxudoynazarov9695@gmail.com>
2018-04-14 14:25:41 +0500

    Shoxrux931-17

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -11,59 +11,39 @@ import views.html._
 import scala.concurrent.ExecutionContext
 
 object HomeController {
-  case class Users(email: String, login: String, createdAt: Date)
+  case class Users(email: String, login: String, password: String, createdAt: Date)
   val user1 = Users(
     email = "asdf@asd.dd",
     login = "admin",
+    password = "az456",
     createdAt = new Date
   )
   val user2 = Users(
     email = "shoxrux0390.@mail.ru",
     login = "shoxrux123",
+    password = "qwe41qa",
     createdAt = new Date
   )
   val user3 = Users(
     email = "shoxruxxudoynazarov0390.@mail.ru",
     login = "banana123",
+    password = "shoxrux0390",
     createdAt = new Date
   )
   val user4 = Users(
     email = "xudoynazarov0390.@mail.ru",
     login = "banana0390",
+    password = "fdsa123",
     createdAt = new Date
   )
   val user5 = Users(
     email = "java0390.@gmail.com",
     login = "scala123",
+    password = "banana4556",
     createdAt = new Date
   )
-  val user6 = Users(
-    email = "scala.@gmail.com",
-    login = "scala321",
-    createdAt = new Date
-  )
-  val user7 = Users(
-    email = "javascala931.@mail.com",
-    login = "person931",
-    createdAt = new Date
-  )
-  val user8 = Users(
-    email = "banana0390.@bk.ru",
-    login = "apple123",
-    createdAt = new Date
-  )
-  val user9 = Users(
-    email = "apple931.@mail.ru",
-    login = "cherry",
-    createdAt = new Date
-  )
-  val user10 = Users(
-    email = "person.@gmail.com",
-    login = "user123",
-    createdAt = new Date
-  )
-  var usersList = List(user1, user2, user3, user4, user5, user6, user7, user8, user9, user10)
-}
+  var usersList = List(user1, user2, user3, user4, user5)
+  }
 
 
 @Singleton
@@ -114,8 +94,8 @@ class HomeController @Inject()(val controllerComponents: ControllerComponents,
     val formParam = request.body.asFormUrlEncoded
     val userLogin = formParam.get("login").headOption
     val password = formParam.get("pwd").headOption
-    val checkLoginDefining = usersList.exists(_.login == userLogin.getOrElse(""))
-    if (checkLoginDefining && password.contains("admin123")) {
+    val authByLoginAndPwd = usersList.exists(user => user.login == userLogin.getOrElse("") && user.password == password.getOrElse("") )
+    if (authByLoginAndPwd) {
       Redirect(routes.HomeController.index()).addingToSession(LoginSessionKey -> userLogin.getOrElse(""))
     } else {
       Redirect(routes.HomeController.login()).flashing("error" -> "Your login or password is incorrect.")
@@ -131,7 +111,8 @@ class HomeController @Inject()(val controllerComponents: ControllerComponents,
     val email = formParam.get("email").head
     val login = formParam.get("login").head
     val pwd = formParam.get("pwd").head
-    usersList = usersList :+ Users(email, login, new Date())
-    Ok("Successfully user added!")
+    usersList = usersList :+ Users(email, login, pwd, new Date())
+    Redirect(routes.HomeController.login())
   }}
-}
+  }
+
